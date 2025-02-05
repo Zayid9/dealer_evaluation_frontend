725eea7b18a526a6755d61b91462b07525c0ea63
zayid9 <maahirzein08@gmail.com>
2025-02-05 10:58:19 +0300

    Added fetch logic in RegisterPage.js

---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,10 @@
+const registerUser = async (userData) => {
+    const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+    return response.json();
+};
